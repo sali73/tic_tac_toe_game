4a8afb3c08e98f1d099ee8ca07b595ce5f87d44c
sali mohamed <sally.elgendy@hotmail.com>
2020-05-04 16:00:52 -0400

    update html file

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('sali')
+const x_class = 'x'
+const circle_class = 'circle'
+const cellElement = document.querySelectorAll('[data-cell]')
+let circleTurn
+
+cellElement.forEach(cell =>{
+    cell.addEventListener('click' , handleClick, {once:true})
+})
+
+function handleClick(e){
+    const cell = e.target
+    const currentClass = circleTurn ? circle_class : x_class 
+    placeMark(cell , currentClass)
+}
+function placeMark(cell , currentClass){
+    cell.classList.add(currentClass)
+}
