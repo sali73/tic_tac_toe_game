fb7d8aa33ea4642a6e95f73b9cf3d9e98cf74dfa
sali mohamed <sally.elgendy@hotmail.com>
2020-05-05 17:57:32 -0400

    create player turn and check all game option with printing msg

--- app.js
+++ app.js
@@ -57,3 +57,100 @@ $("#playButton").click(function (){
 
     setTurn();
 });
+
+//click function on every .col class.
+$(".col").click(function (){
+
+    if(player1Name=="" || player2Name==""){
+        alert("Please set player all the names.");
+        return;
+    }
+
+    var row = $(this).parent().index();
+    var col = $(this).index();
+    //osition is taken
+    if(grid[row][col]!==0){
+        alert("This position is taken. Please try other position.");
+        return;
+    }
+    //play again
+    if(hasWinner==1){
+        alert("Please click play again");
+        return;
+    }
+
+    //checking who is playing now
+    if(turn==player1Name){
+        moveCount++;
+        $(this).text("O");
+        grid[row][col] = 1;
+        var ifWon = winnerCheck(1,player1Name);
+        //if he is not win so threr is 3 other option
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMsg("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player2Name;
+                boardMsg(player2Name+"'s turn now!");
+            }
+            return;    
+        }
+        else{
+            return;
+        }        
+    }
+    // if not player 1turn so its player 2
+    else if(turn==player2Name){
+        moveCount++;
+        $(this).text("X");
+        grid[row][col] = 2;
+        var ifWon = winnerCheck(2,player2Name);
+        //if he is not win so threr is 3 other option
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMsg("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player1Name;
+                boardMsg(player1Name+"'s turn now!");
+            }
+            return;    
+        }
+        else{
+            return;
+        }        
+    }
+});
+//winner check function
+function winnerCheck(n,playerName){
+    if(
+
+        (grid[0][0]==n && grid[0][1]==n && grid[0][2]==n) ||
+        (grid[1][0]==n && grid[1][1]==n && grid[1][2]==n) ||
+        (grid[2][0]==n && grid[2][1]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][0]==n && grid[2][0]==n) ||
+        (grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
+        (grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
+        (grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
+
+
+        ){
+        // msg for winner with his name
+        boardMsg(playerName+" won the game!");
+        hasWinner = 1;
+        moveCount=0;
+        $("#playButton").text("Play again");
+        return true;
+    }
+    return false;
+}
